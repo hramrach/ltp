60ef798dc69df15ae917570aaaa894317fbde415
Michal Suchanek <msuchanek@suse.de>
2019-07-03 20:55:44 +0200

    pcrypt_aead02: test sending signals while adding and removing algorithm
    Signed-off-by: Michal Suchanek <msuchanek@suse.de>

---
+++ testcases/kernel/crypto/pcrypt_aead02.c
@@ -0,0 +1,99 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (c) 2018 SUSE
+ * Author: Nicolai Stange <nstange@suse.de>
+ * LTP conversion: Richard Palethorpe <rpalethorpe@suse.com>
+ *
+ * Originally found by syzkaller:
+ * https://groups.google.com/forum/#!topic/syzkaller-bugs/NKn_ivoPOpk
+ *
+ * Test for CVE-2017-5754 - pcrypt mishandles freeing instances.
+ *
+ * The test works by adding and then removing pcrypt-AEAD instances.
+ * See commit d76c68109f37 crypto: pcrypt - fix freeing pcrypt instances.
+ *
+ * If the bug is present then this will probably crash the kernel, but also
+ * sometimes the test simply times out.
+ */
+
+#include <errno.h>
+#include <time.h>
+#include <signal.h>
+#include <stdlib.h>
+
+#include "tst_test.h"
+#include "tst_safe_net.h"
+#include "tst_taint.h"
+#include "tst_crypto.h"
+
+#define ATTEMPTS 10000
+
+static struct tst_crypto_session ses = TST_CRYPTO_SESSION_INIT;
+static pid_t pid, chld = 0;
+
+static void handle(int signo)
+{
+	(void)signo;
+}
+
+void setup(void)
+{
+	tst_crypto_open(&ses);
+}
+
+void run(void)
+{
+	int i;
+	struct crypto_user_alg a = {
+		.cru_driver_name = "pcrypt(authenc(hmac(sha256-generic),cbc(aes-generic)))",
+		.cru_type = CRYPTO_ALG_TYPE_AEAD,
+		.cru_mask = CRYPTO_ALG_TYPE_MASK,
+	};
+	struct sigaction act = {
+		.sa_handler = &handle,
+		.sa_flags = 0,
+	};
+
+	memset(&act.sa_mask, 0x00, sizeof(sigset_t));
+	if (sigaction(SIGUSR1, &act, NULL))
+		tst_brk(TCONF | TRERRNO, "sigaction");
+
+	pid = getpid();
+	chld = fork();
+	if (!chld) {
+		while (kill(pid, SIGUSR1));
+		exit(0);
+	}
+	if (chld == -1)
+		tst_brk(TCONF | TRERRNO, "fork");
+
+	for (i = 0; i < ATTEMPTS; ++i) {
+		TEST(tst_crypto_add_alg(&ses, &a));
+		if (TST_RET && TST_RET == -ENOENT) {
+			tst_brk(TCONF | TRERRNO,
+				"pcrypt, hmac, sha256, cbc or aes not supported");
+		}
+		if (TST_RET && TST_RET != -EEXIST)
+			tst_brk(TBROK | TRERRNO, "add_alg");
+
+		TEST(tst_crypto_del_alg(&ses, &a));
+		if (TST_RET)
+			tst_brk(TBROK | TRERRNO, "del_alg");
+	}
+
+	tst_res(TPASS, "Nothing bad appears to have happened");
+}
+
+void cleanup(void)
+{
+	tst_crypto_close(&ses);
+	if (chld)
+		kill(SIGKILL, chld);
+}
+
+static struct tst_test test = {
+	.setup = setup,
+	.test_all = run,
+	.cleanup = cleanup,
+	.needs_root = 1,
+};
